7c0685e5b3085def81cec2b3e59ef6966852bd0a
taalaibek <sagynalievv.t@gmail.com>
2025-04-29 15:23:13 +0600

    add request# change status start, complete, cancel

--- api/routes/requestRouter.ts
+++ api/routes/requestRouter.ts
@@ -26,4 +26,56 @@ requestRouter.get('/', async (req, res, next) => {
   }
 });
 
+requestRouter.patch('/:id', async (req, res, next) => {
+  try {
+    const requestId = req.params.id;
+    const { start, complete, cancel } = req.query;
+
+    const request = await Requests.findById(requestId);
+
+    if (!request) {
+      res.status(404).json({ error: 'Not Found Request' });
+    }
+
+    if (start && request) {
+      request.status = 'in_progress';
+      await request.save();
+
+      res.send(request);
+    }
+
+    if (complete && request) {
+      const { solutionText } = req.body;
+
+      if (!solutionText || solutionText.trim().length === 0) {
+        res.status(404).json({ error: 'No Solution Text!' });
+      }
+
+      request.solutionText = solutionText;
+      request.status = 'completed';
+      await request.save();
+
+      res.send(request);
+    }
+
+    if (cancel && request) {
+      const { cancelReason } = req.body;
+
+      if (!cancelReason || cancelReason.trim().length === 0) {
+        res.status(404).json({ error: 'No Cancel Text!' });
+      }
+
+      request.cancelReason = cancelReason;
+      request.status = 'cancelled';
+      await request.save();
+
+      res.send(request);
+    }
+
+    res.status(404).json({ error: 'No such parameter' });
+  } catch (e) {
+    next(e);
+  }
+});
+
 export default requestRouter;
